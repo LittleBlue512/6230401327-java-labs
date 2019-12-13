61f3f4f989ba43621fa1395bda3f3bbe397541f5
LittleBlue512 <chattipoom@outlook.co.th>
2019-12-13 11:09:09 +0700

    Add Athlete Program

--- lab2/sirimul/chattipoom/lab2/Athlete.java
+++ lab2/sirimul/chattipoom/lab2/Athlete.java
@@ -1,14 +1,14 @@
 /**
  * This Athlete program is to accept three arguments: athlete name, the sport name 
-*  that that athlete plays, and the nationality of that athlete.  Its output format is “My 
-*  favorite athlete is <athlete_name> who plays <sport_name> and has 
-* nationality as <athlete_nationality>”  
-*
-* Author: Chattipoom Sirimul
-* ID: 623040132-7
-* Sec: 1
-* Date: December 13, 2019
-*
+ * that that athlete plays, and the nationality of that athlete.  Its output format is “My 
+ * favorite athlete is <athlete_name> who plays <sport_name> and has 
+ * nationality as <athlete_nationality>
+ *
+ * Author: Chattipoom Sirimul
+ * ID: 623040132-7
+ * Sec: 1
+ * Date: December 13, 2019
+ *
 **/
 
 package sirimul.chattipoom.lab2;
@@ -19,7 +19,8 @@ public class Athlete {
             System.out.printf("Athlete <athlete name> <sport name> <nationality>");
         } else {
             String athletename = args[0], sportName = args[1], nationality = args[2];
-            System.out.printf("My favorite athlete is %s who plays %s and has nationality as %s.", athletename, sportName, nationality);
+            System.out.printf("My favorite athlete is %s who plays %s and has nationality as %s.", athletename,
+                    sportName, nationality);
         }
     }
 }
---
+++ lab2/sirimul/chattipoom/lab2/Calculator.java
@@ -0,0 +1,50 @@
+/**
+ * This Calculator program is to accept three arguments: operation operand1 operand2.
+ * It outputs the result of the operation.
+ * The program ends when the user inputs 'q' or 'Q'.
+ *
+ * Author: Chattipoom Sirimul
+ * ID: 623040132-7
+ * Sec: 1
+ * Date: December 13, 2019
+ *
+**/
+
+package sirimul.chattipoom.lab2;
+
+public class Calculator {
+    public static void main(String[] args) {
+        if (args.length != 0 && (args[0].equals("Q") || args[0].equals("q"))) {
+            System.out.println("Quitting the program...");
+            return;
+        } else if (args.length != 3) {
+            System.out.printf("Calculator <operator> <operand1> <operand2>");
+        } else {
+            String operator = args[0], output = "";
+            double operand1 = Double.parseDouble(args[1]), operand2 = Double.parseDouble(args[2]);
+            output = String.format("%.2f %s %.2f = ", operand1, operator, operand2);
+            switch (operator) {
+            case "+":
+                output += String.format("%.2f", operand1 + operand2);
+                break;
+            case "-":
+                output += String.format("%.2f", operand1 - operand2);
+                break;
+            case "x":
+                output += String.format("%.2f", operand1 * operand2);
+                break;
+            case "/":
+                if (operand2 == 0) {
+                    output = "The second operand cannot be zero.";
+                } else {
+                    output += String.format("%.2f", operand1 / operand2);
+                }
+                break;
+            default:
+                System.out.println("Valid operators: + - x /");
+                return;
+            }
+            System.out.println(output);
+        }
+    }
+}
