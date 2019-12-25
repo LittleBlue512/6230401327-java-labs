0742135b4e6ee7c978a10abc54520cd8a1951731
LittleBlue512 <chattipoom@outlook.co.th>
2019-12-25 14:39:24 +0700

    Add Exercises

---
+++ Exercises/Overloading.java
@@ -0,0 +1,55 @@
+
+/**
+ * This program is to use overloading methods to handle multiple cases of input.
+ * 
+ * Author: Chattipoom Sirimul
+ * Date: December 25, 2019
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Overloading {
+    public static void main(String[] args) {
+        display("Hello");
+        display("Hello", "Bye");
+        display(2, 3);
+        display(2, 3, 4);
+    }
+
+    // Handle single string argument
+    private static void display(String str) {
+        System.out.println(str);
+    }
+
+    // handle multiple string arguments
+    private static void display(String... strs) {
+        String output = "";
+
+        // Format the output
+        output = String.join(" and ", strs);
+
+        System.out.println(output);
+    }
+
+    // Handle multiple int arguments
+    private static void display(int... nums) {
+        int sum = 0;
+        String output = "";
+        List<String> numsChars = new ArrayList<>();
+
+        // Iterate through the array of arguments
+        for (int num : nums) {
+            // Add the value of each element to sum
+            sum += num;
+            // Convert each element to String and add to the list of String.
+            numsChars.add(Integer.toString(num));
+        }
+
+        // Format the output
+        output = String.join(" + ", numsChars) + String.format(" = %d", sum);
+
+        // Print
+        System.out.println(output);
+    }
+}
---
+++ Exercises/Recursive.java
@@ -0,0 +1,52 @@
+
+/**
+ * This program is to find a(n) which a(n) = a(n-1) + 2 and a(1) = 3. User
+ * enters value of n and programs will show the result of a(n).
+ * 
+ * Author: Chattipoom Sirimul
+ * Date: December 25, 2019
+ */
+
+import java.util.Scanner;
+
+public class Recursive {
+    public static void main(String[] args) {
+        int n = 0;
+        String userInput;
+        Scanner userScanner = new Scanner(System.in);
+
+        // Get a valid input
+        while (true) {
+            // Get an input
+            System.out.print("Enter value of n: ");
+            userInput = userScanner.nextLine();
+
+            // Check if input is an integer
+            try {
+                n = Integer.parseInt(userInput);
+            } catch (NumberFormatException ex) {
+                System.out.println("Invalid Input: Please enter an integer only.");
+                continue;
+            }
+
+            // Check if input is greater than 1
+            if (n < 1) {
+                System.out.println("Invalid Input: Value of n must be greater or equal 1.");
+                continue;
+            }
+
+            break;
+        }
+
+        // Display the result
+        System.out.printf("a(%d) = %d", n, rec(n));
+
+        // Close the scanner
+        userScanner.close();
+    }
+
+    // Get a(n)
+    private static int rec(int n) {
+        return n <= 1 ? 3 : rec(n - 1) + 2;
+    }
+}
