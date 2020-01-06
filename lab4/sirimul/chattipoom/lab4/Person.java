0b1aff2cd7b36fa53dc75711d889441a1ac86fd8
Chattipoom <LittleBlue512@users.noreply.github.com>
2020-01-06 13:34:30 +0700

    Add "lab4"

---
+++ lab4/sirimul/chattipoom/lab4/Person.java
@@ -0,0 +1,70 @@
+public class Person {
+    private String name;
+    private double height;
+    private double weight;
+    private static int numberOfPerson = 0;
+
+    public Person() {
+        this.name = "unknown";
+        this.weight = 0.0f;
+        this.height = 0.0f;
+        Person.numberOfPerson++;
+    }
+
+    public Person(String _name) {
+        this.name = _name;
+        this.weight = 0.0f;
+        this.height = 0.0f;
+        Person.numberOfPerson++;
+    }
+
+    public Person(String _name, double _height, double _weight) {
+        this.name = _name;
+        this.height = _height; // cm
+        this.weight = _weight; // kg
+        Person.numberOfPerson++;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String _name) {
+        this.name = _name;
+    }
+
+    public double getHeight() {
+        return this.height;
+    }
+
+    public void setHeight(double _height) {
+        this.height = _height;
+    }
+
+    public double getWeight() {
+        return this.weight;
+    }
+
+    public void setWeight(double _weight) {
+        this.weight = _weight;
+    }
+
+    @Override
+    public String toString() {
+        String output = "";
+        output += String.format("Name: %s\n", this.name);
+        output += String.format("Name: %.2f\n", this.height);
+        output += String.format("Name: %.2f\n", this.weight);
+        return output;
+    }
+
+    public String getBMI() {
+        double height = this.getHeight() / 100; // cm -> m
+        double weight = this.getWeight();
+        return String.format("%.2f", weight / (height * height));
+    }
+
+    public static int getNumberOfPerson() {
+        return Person.numberOfPerson;
+    }
+}
