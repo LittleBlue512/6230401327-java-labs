8825a8f7f4e65cf832c4dd7c0c3647febd72051a
LittleBlue512 <chattipoom@outlook.co.th>
2020-01-19 21:29:55 +0700

    Add lab6

---
+++ lab6/sirimul/chattipoom/lab6/MySimpleWindow.java
@@ -0,0 +1,87 @@
+/**
+ * This program is to create a class called MySimpleWindow which extends from class JFrame.
+ * The class has a panel and two buttons. It also has methods to add components and set the features of the frame.
+ * 
+ * Author: Chattipoom Sirimul
+ * ID: 623040132-7
+ * Section: 1
+ * Date: January 19, 2020
+ */
+
+package sirimul.chattipoom.lab6;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+
+import javax.swing.SwingUtilities;
+
+public class MySimpleWindow extends JFrame {
+    // Quick fix warning: Java(536871008) ~ Searched from Google.
+    private static final long serialVersionUID = -5235778330626143850L;
+
+    // Static variables for string in buttons.
+    static public String cancelButtonString = "Cancel";
+    static public String okButtonString = "Ok";
+
+    private String frameTitle;
+
+    // Components
+    private JFrame mainFrame;
+    private JPanel mainPanel;
+    private JButton cancelButton;
+    private JButton okButton;
+
+    // A constructor with one parameter: frame's title.
+    public MySimpleWindow(String _frameTitle) {
+        // Set the frame title of this object.
+        this.frameTitle = _frameTitle;
+        // Create a JFrame for this object.
+        this.mainFrame = new JFrame();
+    }
+
+    // A method for adding components to the main frame.
+    protected void addComponents() {
+        // Create a JPanel for this object.
+        this.mainPanel = new JPanel();
+
+        // Create a JButtons for this object.
+        this.cancelButton = new JButton(cancelButtonString);
+        this.okButton = new JButton(okButtonString);
+
+        // Add the buttons to the main panel.
+        this.mainPanel.add(this.cancelButton);
+        this.mainPanel.add(this.okButton);
+
+        // Add the main panel to the main frame.
+        this.mainFrame.add(this.mainPanel);
+    }
+
+    // A method for setting frame features.
+    protected void setFrameFeatures() {
+        // Set the JFrame's title.
+        this.mainFrame.setTitle(this.frameTitle);
+        // Set the JFrame's sizes automatically by using pack().
+        this.mainFrame.pack();
+        // Make the JFrame visible.
+        this.mainFrame.setVisible(true);
+        // The program exit when the user clicks to close a frame.
+        this.mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Set the window location at the center of the screen.
+        this.mainFrame.setLocationRelativeTo(null);
+    }
+
+    public static void createAndShowGUI() {
+        MySimpleWindow msw = new MySimpleWindow("My Simple Window");
+        msw.addComponents();
+        msw.setFrameFeatures();
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+}
