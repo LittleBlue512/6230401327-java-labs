ad69aa2904522cb4e041e4047a016391b54b51dd
LittleBlue512 <chattipoom@outlook.co.th>
2020-03-21 17:28:16 +0700

    Update lab10.

--- labs/sirimul/chattipoom/lab10/PersonFormV14.java
+++ labs/sirimul/chattipoom/lab10/PersonFormV14.java
@@ -1,6 +1,6 @@
 /**
  * This class is to implement open/save function.
- * The program saves Object  to a ".well" file and also read Object from a ".well" file.
+ * The program saves bbject  to a ".well" file and also read bbject from a ".well" file.
  * In this case, my object is ArrayList<Person>.
  * 
  * Author: Chattipoom Sirimul
@@ -27,7 +27,7 @@ public class PersonFormV14 extends PersonFormV13 {
     private static final long serialVersionUID = 1L;
 
     protected static final String FILE_EXTENSION = "well";
-    protected static final String FILE_EXTENSION_ERR = "Error: Cannot save file. The selected file must be a \".well\" file.";
+    protected static final String FILE_EXTENSION_ERR = "Error: Cannot save/open file. The selected file must be a \".well\" file.";
 
     public PersonFormV14(String string) {
         super(string);
